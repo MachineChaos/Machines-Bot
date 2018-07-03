5ffb6ae9cbdc35c46bebdb92f0d8fc25844b859d
MachineChaos <40797980+MachineChaos@users.noreply.github.com>
2018-07-03 18:46:52 +0100

    Second commit

---
+++ MachinesBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MachinesBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ MachinesBot/Storage/IDataStorage.cs
@@ -0,0 +1,10 @@
+namespace MachinesBot
+{
+    public interface IDataStorage
+    {
+        void StoreObject(object obj, string key);
+
+        T RestoreObject<T>(string key);
+
+    }
+}
