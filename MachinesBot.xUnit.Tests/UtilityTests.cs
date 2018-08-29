2ab1661ac3a36c5f84f38d49f1bd00744c0ca549
MachineChaos <40797980+MachineChaos@users.noreply.github.com>
2018-08-29 01:18:45 +0100

    Implement first Unit Test with xUnit

---
+++ MachinesBot.xUnit.Tests/UtilityTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using MachinesBot;
+
+namespace MachinesBot.xUnit.Tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            const int expected = 5;
+
+            var actual = Utilities.MyUtility(expected);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ MachinesBot/Utilities.cs
@@ -0,0 +1,10 @@
+namespace MachinesBot
+{
+    public class Utilities
+    {
+        public static int MyUtility(int i)
+        {
+            return i;
+        }
+    }
+}
